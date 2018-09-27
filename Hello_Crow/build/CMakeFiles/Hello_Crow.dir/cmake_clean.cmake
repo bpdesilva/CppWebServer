93c7a1542480e5ca247903737ea53d00fa53dc34
bpdesilva <buwanekapasan@gmail.com>
2018-09-28 05:18:41 +0530

    Init

---
+++ Hello_Crow/build/CMakeFiles/Hello_Crow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello_Crow.dir/main.cpp.o"
+  "Hello_Crow.pdb"
+  "Hello_Crow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hello_Crow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
